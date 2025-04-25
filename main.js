e5ae96522d30fdbf8366322c405fc7cf809955b6
joark_iv <45000961+Joark-iv@users.noreply.github.com>
2025-04-25 10:05:30 +0100

    added function to get Human Input

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 //create function to get computer random choice for the game
-function getComputerChoice(...choices){
+function getComputerChoice(...choices) {
     const randomIndex = Math.floor(Math.random() * choices.length);
     const randomChoice = choices[randomIndex];
-console.log(randomChoice);
+    console.log(randomChoice);
+}
+getComputerChoice('rock', 'paper', 'scissors')
+
+//logic to get human Input 
+function getHumanChoice() {
+    let humanInput = prompt(`rock, paper or scissors?`);
+    if (humanInput !== null) {
+        humanInput = humanInput.toLowerCase();
+    }
+
+    console.log(humanInput);
+
 }
 
-getComputerChoice('rock','paper','scissors')
+getHumanChoice();
